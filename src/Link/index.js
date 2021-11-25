7436e74d58cf67795f99ee08b68736521793383f
mahesh-muttinti <maheshmuttinti@gmail.com>
2021-11-25 12:37:54 +0530

    add href prop to Link component

--- src/Link/index.js
+++ src/Link/index.js
@@ -8,7 +8,7 @@ export default function index({ children, ...props }) {
   const anchorTagStyles = props.anchorTagStyles || 'font-normal text-xs text-blue-500 hover:text-blue-600 dark:text-white cursor-pointer';
 
   return (
-    <a className={classNames(anchorTagStyles)}>
+    <a className={classNames(anchorTagStyles)} href={props.href}>
       {children}
     </a>
   );
